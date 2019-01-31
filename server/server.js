211ea4549e71dcd7746dd7c10cd51a502621ae96
blessed-tawanda <blessedmahuni@gmail.com>
2019-01-31 15:42:18 +0200

    Added PATCH /todo/:id to update todos

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const {mongoose} = require('./db/mongoose')
 const {ObjectID} = require('mongodb')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
+const _ = require('lodash')
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -64,6 +65,32 @@ app.delete('/todos/:id', (req,res) => {
 
   }).catch((err) => {
     res.status(400).send('')
+  })
+
+})
+
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id
+  var body = _.pick(req.body,['text','completed'])
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send('')
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send()
+    }
+    res.send({todo});
+  }).catch((err) => {
+    res.status(400).send()
   })
 
 })
